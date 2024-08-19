2e9499fb2e1adca802b4a9cd54f9937a7d6545e5
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-08-19 14:50:05 +0700

    refactor: Refactor and UI enhancements:
    - Removed unused imports and game logic from `App.vue` to simplify the codebase.
    - Applied new styling for responsive and user-friendly design.
    - Updated the title font to 'Irish Grover' and button styles for a consistent visual theme.
    - Reorganized layout components for better alignment and spacing.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref, watch } from 'vue';
 import useRouter from './libs/router';
 import useGameStore from './stores/game-store';
 
@@ -9,90 +8,60 @@ import {
 	MODE_COMPLETED_PRIZE,
 	WORDS_PER_ROUND,
 } from './configs/game-config';
-import { book } from './vocabulary.json';
 
 const { navigate, playRoutes, on } = useRouter(pages, FIRST_PAGE_NAME);
-const {
-	onPage,
-	letterBoxes,
-	resetInGameState,
-	turnToPage,
-	stackLetter,
-	clearStack,
-	validateLetter,
-	saveCompletedWord,
-	receiveHint,
-	useHint,
-	isLastPage,
-	isBookCompleted,
-	currentCharArray,
-} = useGameStore(book, { MODE_COMPLETED_PRIZE, WORDS_PER_ROUND });
-
-const clickedLetters = ref({});
-
-watch(
-	() => clickedLetters,
-	() => {
-		const isAllClicked =
-			Object.values(clickedLetters.value).length ===
-				currentCharArray.value.length && currentCharArray.value.length > 0;
-
-		if (isAllClicked) {
-			if (validateLetter()) {
-				saveCompletedWord();
-				if (!isLastPage()) {
-					turnToPage({ mode: onPage.mode, index: onPage.index + 1 });
-					playRoutes({ pageNames: [PAGE.LEVEL_COMPLETED, PAGE.GAME] });
-				} else {
-					playRoutes({
-						pageNames: [
-							PAGE.LEVEL_COMPLETED,
-							PAGE.MODE_COMPLETED,
-							isBookCompleted() ? PAGE.GAME_COMPLETED : '',
-							PAGE.MODE,
-						],
-					});
-					receiveHint();
-					resetInGameState();
-					return;
-				}
-			}
-			clearStack();
-			clickedLetters.value = {};
-		}
-	},
-	{ deep: true },
-);
 </script>
 
 <template>
-	<div>
+	<div class="h-screen flex items-center justify-center bg-[#FEF9EF] p-4">
 		<!-- Home View -->
 		<div
 			id="home-view"
 			v-if="on(PAGE.HOME)"
+			class="h-full flex flex-col items-center justify-center gap-[10%] w-full md:w-[70%]"
 		>
-			<h1 id="game-title">CLICK WORD</h1>
-			<button @click="navigate(PAGE.MODE)">
-				<img
-					src="./assets/play-button.png"
-					alt="play-button"
-				/>
-			</button>
-			<div id="progression-badge">
-				<div>
+			<h1
+				class="text-[12vw] sm:text-[10vw] md:text-[8vw] lg:text-[6vw] text-[#237C9D] title"
+			>
+				CLICK WORD
+			</h1>
+			<div class="mt-8">
+				<button @click="navigate(PAGE.MODE)">
 					<img
-						src="./assets/trophy.png"
-						alt="success-badge"
+						src="./assets/play-button.png"
+						alt="Play"
+						class="w-[30vw] sm:w-[20vw] md:w-[15vw] lg:w-[10vw] mx-auto"
 					/>
-					<p>Success</p>
+				</button>
+			</div>
+			<div class="flex justify-center gap-[10vw] mt-12">
+				<div class="flex flex-col items-center space-y-4">
+					<div>
+						<img
+							src="./assets/trophy.png"
+							alt="Trophy"
+							class="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20"
+						/>
+					</div>
+					<div
+						class="text-lg sm:text-xl md:text-2xl lg:text-3xl text-white bg-[#19C3B2] px-4 py-2 rounded-xl"
+					>
+						Success ({{}})
+					</div>
 				</div>
-				<div>
-					<img
-						src="./assets/lightbulb.png"
-						alt="hints-badge"
-					/>
-					<p>Hints</p>
+				<div class="flex flex-col items-center space-y-4">
+					<div>
+						<img
+							src="./assets/light-bulb.png"
+							alt="Light Bulb"
+							class="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20"
+						/>
+					</div>
+					<div
+						class="text-lg sm:text-xl md:text-2xl lg:text-3xl text-white bg-[#FF9090] px-4 py-2 rounded-xl"
+					>
+						Hints ({{}})
+					</div>
 				</div>
 			</div>
 		</div>
@@ -100,80 +69,39 @@ watch(
 		<div
 			id="mode-view"
 			v-if="on(PAGE.MODE)"
+			class="h-full flex flex-col items-center justify-center gap-[10%] w-full md:w-[70%]"
 		>
-			<h1>MODE</h1>
-			<div id="mode-buttons-container">
+			<h1
+				class="text-[12vw] sm:text-[10vw] md:text-[8vw] lg:text-[6vw] text-[#237C9D] title"
+			>
+				MODE
+			</h1>
+			<div class="flex flex-col items-center gap-8 w-full">
 				<button
+					class="text-[#FEF9EF] w-[60vw] sm:w-[40vw] md:w-[30vw] lg:w-[20vw] h-[12vw] sm:h-[10vw] md:h-[8vw] lg:h-[6vw] px-4 py-2 rounded-xl text-lg sm:text-xl md:text-2xl lg:text-3xl"
+					:style="{ backgroundColor: `${difficulty.primaryColor}` }"
 					v-for="(difficulty, index) in difficulties"
 					:key="index"
-					@click="
-						() => {
-							turnToPage({ mode: difficulty.name, index: onPage.index });
-							navigate(PAGE.GAME);
-						}
-					"
+					@click="navigate(PAGE.GAME)"
 				>
-					{{ difficulty.name }}
+					{{
+						difficulty.name.charAt(0).toUpperCase() + difficulty.name.slice(1)
+					}}
 				</button>
 			</div>
-		</div>
-		<!-- Game View -->
-		<div
-			id="game-view"
-			v-if="on(PAGE.GAME)"
-		>
-			<div>Progress Bar Placeholder</div>
-			<div id="letters-group-container">
-				<button
-					v-for="(letter, index) in currentCharArray"
-					:key="index"
-					@click="
-						() => {
-							clickedLetters[index] = true;
-							stackLetter(letter);
-						}
-					"
-					:disabled="clickedLetters[index]"
-				>
-					{{ letter }}
-				</button>
-			</div>
-			<div id="letters-box-container">
-				<div
-					v-for="n in currentCharArray.length"
-					:key="n"
-					class="border-slate-700 border-2 w-8 h-8"
-				>
-					{{ letterBoxes[n - 1] }}
-				</div>
-			</div>
-			<div id="cta-container">
-				<button
-					@click="
-						() => {
-							clearStack();
-							clickedLetters = {};
-						}
-					"
-				>
-					Clear
-				</button>
-				<button @click="useHint(clickedLetters)">Hints</button>
-			</div>
-		</div>
-		<!-- Level Completed View -->
-		<div
-			id="level-completed-view"
-			v-if="on(PAGE.LEVEL_COMPLETED)"
-		>
-			Level Completed View
-		</div>
-		<!-- Mode Completed View -->
-		<div
-			id="mode-completed-view"
-			v-if="on(PAGE.MODE_COMPLETED)"
-		>
-			Mode Completed View
 		</div>
 	</div>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Irish+Grover&family=Itim&display=swap');
+
+* {
+	font-family: 'Itim', sans-serif;
+}
+
+.title {
+	font-family: 'Irish Grover', cursive;
+}
+</style>
